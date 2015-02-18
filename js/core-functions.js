7fe3c56eae65eadb673b17e15a6883649687092f
Marta Nabozny <martastrzet@gmail.com>
2015-02-18 12:39:56 +0100

    debugging option

--- js/core-functions.js
+++ js/core-functions.js
@@ -1,4 +1,12 @@
 function request(func, args, callback) {
+    var msginfo = $('<div class="alert alert-info errormessage" style="position: absolute; display: none;">');
+    if (window.debug) {
+        var close = $('<button type="button" class="close" data-dismiss="alert">x</button>');
+        msginfo.append(close);
+        msginfo.append(func);
+        msginfo.appendTo($('body')).fadeIn(100);
+    }
+
     $.ajax({
         type: "POST",
         url: window.coreUrl + func,
@@ -10,14 +18,21 @@ function request(func, args, callback) {
                 return;
             }
             var response = $.parseJSON(xhr.responseText);
+
             if (response.status != "ok") {
+                msginfo.removeClass('alert-info');
+                msginfo.addClass('alert-danger');
+
                 var message = $('<div class="alert alert-danger errormessage" style="position: absolute; display: none;">');
                 var close = $('<button type="button" class="close" data-dismiss="alert">x</button>');
                 message.append(close);
                 message.append(response.status);
                 message.appendTo($('body')).fadeIn(300).delay(5000).fadeOut(300);
                 return;
+            } else if (window.debug) {
+                msginfo.fadeOut();
             }
+
             callback(response.data);
         },
         dataType: "application/json"
--- settings.js
+++ settings.js
@@ -1,2 +1,5 @@
 // OverCluster-Core url (Core API)
 window.coreUrl = "http://192.245.169.152:8000/";
+
+// Enable debugging messages
+window.debug = 1;
