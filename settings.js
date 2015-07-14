bf3ccfd59c778ac443ca7ef8d7f1eacfefe924b7
Marta Nabozny <martastrzet@gmail.com>
2015-07-14 12:04:26 +0200

    controllers

--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
 window.coreUrl = "http://10.100.0.200:8000/";
 window.debug = true;
-window.modules = ['home', 'vm', 'image', 'network', 'coretalk', 'account'];
+
+// Installed applications. Put 'home' controller at end of this list. Otherwise, it will catch all urls
+window.modules = ['vm', 'image', 'network', 'coretalk', 'account', 'home'];
