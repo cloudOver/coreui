fe6f6f43c3efead0411c9e0f7462a9a8f6f1ad40
Marta Nabozny <martastrzet@gmail.com>
2015-08-26 19:31:09 +0200

    static files moved to repository

--- settings.js
+++ settings.js
@@ -2,4 +2,4 @@ window.coreUrl = "http://10.100.0.200:8000/";
 window.debug = true;
 
 // Installed applications. Put 'home' controller at end of this list. Otherwise, it will catch all urls
-window.modules = ['vm', 'image', 'network', 'coretalk', 'vpn', 'account', 'home'];
+window.modules = ['vm', 'image', 'network', 'coretalk', 'account', 'home'];
