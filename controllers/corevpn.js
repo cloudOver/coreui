a1f47871cb27a1e0bd0b71ac22f94c789dd2becf
Marta Nabozny <marta.nabozny@cloudover.io>
2016-03-01 18:15:38 +0100

    CloudInit wizard

--- controllers/corevpn.js
+++ controllers/corevpn.js
@@ -1,11 +1,11 @@
-window.app.controller('UserdataListCtrl', function ($scope, $location, $http) {
-    var model = 'userdata';
+window.app.controller('CoreVpnListCtrl', function ($scope, $location, $http) {
+    var model = 'vpn';
     $scope.obj_edit = function(obj) {
-        $location.path('/coretalk/' + model + '/' + obj.id + '/');
+        $location.path('/corevpn/' + model + '/' + obj.id + '/');
         $scope.$apply();
     };
     $scope.obj_create = function() {
-        $location.path('/coretalk/' + model + '/create/');
+        $location.path('/corevpn/' + model + '/create/');
         $scope.$apply();
     };
     request('/api/' + model + '/get_list/', {token: $.cookie("core_token")}, function(objs) {
@@ -14,105 +14,14 @@ window.app.controller('UserdataListCtrl', function ($scope, $location, $http) {
     });
 });
 
-window.app.controller('UserdataWizardCtrl', function ($scope, $location, $http) {
-    $scope.packages = [];
-    $scope.cacerts = [];
-    $scope.commands = [];
-    $scope.users = [];
-    $scope.name = '';
-    $scope.update = 'False';
-    $scope.upgrade = 'False';
-    $scope.remove_defaults = 'False';
 
-    $scope.addCommand = function() {
-        $scope.commands.push({id: $scope.commands.length, value: ''});
-    };
-
-    $scope.addCacert = function() {
-        $scope.cacerts.push({id: $scope.cacerts.length, value: ''});
-    };
-
-    $scope.addPackage = function() {
-        $scope.packages.push({id: $scope.packages.length, value: ''});
-    };
-
-    $scope.addUser = function() {
-        $scope.users.push({ssh_keys: []});
-    };
-
-    $scope.addKey = function(user) {
-        user.ssh_keys.push({id: user.ssh_keys.length, value: ''});
-    };
-
-    $scope.saveUserdata = function() {
-        d = {};
-
-        if ($scope.users.length > 0) {
-            d['users'] = [];
-            for (i = 0; i < $scope.users.length; i++) {
-                user = {};
-                user['name'] = $scope.users[i].name;
-                user['password'] = $scope.users[i].password;
-                user['primary-group'] = $scope.users[i].primary_group;
-                user['groups'] = $scope.users[i].groups;
-                user['system'] = $scope.users[i].system;
-                user['lock-password'] = $scope.users[i].lock_password;
-                keys = [];
-                for (j = 0; j < $scope.users[i].ssh_keys.length; j++) {
-                    keys.push($scope.users[i].ssh_keys[j].value);
-                }
-                user['ssh-authorized-keys'] = keys;
-                d['users'].push(user);
-            }
-        }
-
-        if ($scope.packages.length > 0) {
-            d['packages'] = []
-            for (i = 0; i < $scope.packages.length; i++) {
-                d['packages'].push($scope.pakcages[i].value);
-            }
-        }
-
-        if ($scope.cacerts.length > 0) {
-            d['ca-certs'] = {}
-            d['ca-certs']['remove-defaults'] = $scope.remove_defaults;
-            d['ca-certs']['trusted'] = [];
-            for (i = 0; i < $scope.cacerts.length; i++) {
-                d['ca-certs']['trusted'].push($scope.cacerts[i].value);
-            }
-        }
-
-        if ($scope.commands.length > 0) {
-            d['bootcmd'] = []
-            for (i = 0; i < $scope.commands.length; i++) {
-                d['bootcmd'].push($scope.commands[i].value);
-            }
-        }
-
-        d['package_update'] = $scope.update;
-        d['package_upgrade'] = $scope.upgrade;
-
-        console.log(d);
-
-        request('/api/userdata/create/', {token: $.cookie("core_token"),
-            name: $scope.name,
-            data: d,
-            convert_from: 'native',
-        }, function(resp) {
-            $location.path('/coretalk/userdata/');
-            $scope.$apply();
-        });
-    };
-});
-
-
-window.app.controller('UserdataCreateCtrl', function ($scope, $location, $http) {
+window.app.controller('CoreVpnCreateCtrl', function ($scope, $location, $http) {
     $scope.script = '';
     $scope.name = '';
     $scope.create = function() {
-        request('/api/userdata/create/', {token: $.cookie("core_token"),
+        request('/vpn/network/create/', {token: $.cookie("core_token"),
             name: $scope.name,
-            data: $scope.script
+
         }, function(resp) {
             $location.path('/coretalk/userdata/');
             $scope.$apply();
@@ -231,11 +140,7 @@ window.app.controller('SshkeyEditCtrl', function ($scope, $location, $http, $rou
 
 
 window.app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when("/coretalk/userdata/", {templateUrl: "views/coretalk/userdata_list.html", controller: "UserdataListCtrl"})
-        .when("/coretalk/userdata/wizard/", {templateUrl: "views/coretalk/userdata_wizard.html", controller: "UserdataWizardCtrl"})
-        .when("/coretalk/userdata/create/", {templateUrl: "views/coretalk/userdata_create.html", controller: "UserdataCreateCtrl"})
-        .when("/coretalk/userdata/:id/", {templateUrl: "views/coretalk/userdata_edit.html", controller: "UserdataEditCtrl"})
-        .when("/coretalk/sshkey/", {templateUrl: "views/coretalk/sshkey_list.html", controller: "SshkeyListCtrl"})
-        .when("/coretalk/sshkey/create/", {templateUrl: "views/coretalk/sshkey_create.html", controller: "SshkeyCreateCtrl"})
-        .when("/coretalk/sshkey/:id/", {templateUrl: "views/coretalk/sshkey_edit.html", controller: "SshkeyEditCtrl"})
+    $routeProvider.when("/corevpn/", {templateUrl: "views/corevpn/vpn_list.html", controller: "CoreVpnListCtrl"})
+        .when("/corevpn/create/", {templateUrl: "views/coretalk/userdata_create.html", controller: "UserdataCreateCtrl"})
+        .when("/corevpn/:id/", {templateUrl: "views/coretalk/userdata_edit.html", controller: "UserdataEditCtrl"})
 }]);
--- settings.js
+++ settings.js
@@ -2,4 +2,4 @@ window.coreUrl = "http://92.222.21.200:8000/";
 window.debug = true;
 
 // Installed applications. Put 'home' controller at end of this list. Otherwise, it will catch all urls
-window.modules = ['vm', 'image', 'network', 'coretalk', 'account', 'home', 'dhcp'];
+window.modules = ['vm', 'image', 'network', 'coretalk', 'account', 'home', 'coredhcp'];
