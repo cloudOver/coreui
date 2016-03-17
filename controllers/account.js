8844ebc9f8e840f2caa1cf4e79bfe9f43ba18184
Marta Nabozny <marta.nabozny@cloudover.io>
2016-03-17 10:08:31 +0100

    Terms of use

--- controllers/account.js
+++ controllers/account.js
@@ -1,3 +1,22 @@
+/*
+Copyright (c) 2014-2016 cloudover.io ltd.
+
+This file is part of cloudover.coreCluster project.
+
+cloudover.coreCluster is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 window.app.controller('TokenListCtrl', function ($scope, $location, $http) {
     var model = 'userdata';
     $scope.obj_create = function() {
